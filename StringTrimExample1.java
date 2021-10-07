1f49dbecf7ae1986e9307b5294cd1b8d286eaa23
narendranvel1990 <63440628+narendranvel1990@users.noreply.github.com>
2021-10-07 22:08:21 +0530

    Add files via upload

---
+++ StringTrimExample1.java
@@ -0,0 +1,9 @@
+public class StringTrimExample1
+{
+	public static void main(String args[])
+{
+String s1="hello string";
+System.out.println(s1.endsWith("hello string"));//without trim()
+//System.out.println(s1.trim()+"javatpoint");//with trim()
+}
+}
